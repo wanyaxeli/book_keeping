b956cc17b01bc0fa9ef250e8f03f87187ce99818
Wanyaxeli <eliwanyax@gmail.com>
2023-11-13 19:32:01 +0300

    added gh pages

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
   <Provider store={store}>
-    <Router>
+    <Router basename='/book_keeping_server'>
     <App />
     </Router>
   </Provider>
